b0a4ed653783743b2cab811e3c4b908fa2e43362
Scott Creeley <screeley@redhat.com>
2019-05-02 13:58:48 -0400

    Create push-operator-registry.sh

---
+++ scripts/push-operator-registry.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# This script builds and pushes an operator registry image and pushes to
+# quay.io/$registry/federation-operator-registry
+#
+# This script should be run from the root directory of the
+# federation-v2-operator repository.
+
+REGISTRY=${REGISTRY:-$1}
+if [[ -z "${REGISTRY}" ]]; then
+  echo "registry must be set by running \`push-operator-registry <registry>\` or by setting \$REGISTRY"
+  exit 1
+fi
+new_image_name=quay.io/$REGISTRY/aws-s3-provisioner:v1.0.0
+
+IMAGE=${IMAGE:-""}
+if [[ -n "${IMAGE}" ]]; then
+  new_image_name=$IMAGE
+fi
+
+UPSTREAM=${UPSTREAM:-}
+dockerfile=olm-testing/Dockerfile
+if [[ -n "$UPSTREAM" ]]; then
+  dockerfile=olm-testing/Dockerfile.upstream
+fi
+
+tag="quay.io/$REGISTRY/federation-operator-registry:v4.0.0"
+echo "Building operator registry with tag $tag"
+docker build . -f $dockerfile -t $tag --build-arg new_image_name=$new_image_name
+echo "Pushing $tag"
+docker push $tag
